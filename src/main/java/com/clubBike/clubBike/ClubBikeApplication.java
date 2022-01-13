1edb087612207432021a2ed1be24968d3e8164f0
ChoiHongSik <94hongsik@gmail.com>
2022-01-14 00:26:02 +0900

    2022.01.14 초기 프로젝트 생성

---
+++ src/main/java/com/clubBike/clubBike/ClubBikeApplication.java
@@ -0,0 +1,13 @@
+package com.clubBike.clubBike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubBikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clubBike/clubBike/ClubBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubBike.clubBike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
